c562d4ff2731b08b19bb381bf798d17360b8590c
Tonyy456 <ajdalesandro0115@gmail.com>
2024-04-12 14:35:04 -0400

    fixing server routing issue?

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const path = require('path');
 const dayjs = require('dayjs');
 const app = express();
-const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, '../../frontend/build')));
-app.get('*', (req, res) => {
-    res.sendFile(
-        path.resolve(__dirname, '../../frontend/build/index.html')
-    )
-})
+app.use(express.static(path.join(__dirname, '../../frontend/build')))
+app.use(express.json());
+app.get("*", (req,res) => { 
+  res.sendFile(
+      path.resolve(__dirname, '../../frontend/build/index.html')
+); }) //wild card. redirect unknown pages to react...
+
+// start server!
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    const date = dayjs();
-    console.log(`Listening on port ${PORT} at ${date.format('MM/DD/YY @ hh:mm:ss UTC')}`);
+    const day = dayjs();
+    console.log(`Listening on port ${PORT} ${day.format('MM/DD/YY @ hh:mm:ss')}`);
 })
