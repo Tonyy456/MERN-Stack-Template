9ede5623123253009b0448fa887acdad26ddc54a
Tonyy456 <ajdalesandro0115@gmail.com>
2024-04-12 16:42:05 -0400

    fixing 404 issue

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -6,11 +6,5 @@ const myAPI = require('../api');
 const router = express.Router();
 
 router.use('/api', myAPI);
-router.get("*", (req,res) => {
-        res.sendFile(
-            path.resolve(__dirname, "../../../frontend/build/index.html")
-        ); 
-    }
-) //wild card. redirect unknown pages to react...
         
 module.exports = router
